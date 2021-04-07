8fbccfdde8595c92d2ccd4fe58aa360e79a7e55d
kkokkojeong <comgraphics11@gmail.com>
2021-04-07 16:51:50 +0900

    feat: #14 video markup

--- src/components/shared/content/video-item/VideoItem.tsx
+++ src/components/shared/content/video-item/VideoItem.tsx
@@ -32,7 +32,12 @@ const VideoItem = ({
     count,
     date,
 }: Props) => {
-    const viewCount = typeof count === 'string' ? parseInt(count, 10) : count;
+    const viewCount = (() => {
+        const hits = typeof count === 'string' ? parseInt(count, 10) : count;
+        if (hits > 10000) return `${Math.round(hits / 10000)}만명`;
+        if (hits > 1000) return `${Math.round(hits / 1000)}천명`;
+        return `${hits}명`;
+    })();
     // TODO: 현재 날짜와 비교해서 날짜 구하는 계산 추가
     const latestDate = date;
 
@@ -45,30 +50,30 @@ const VideoItem = ({
                         <img src={thumbnail} width="36" height="36" alt="" className={css.video_item_player_thumbnail} />
                         <span>{duration}</span>
                     </a>
-                </div>
-                {/* 영상 우측 클릭 영역 - 나중에 볼 동영상, 목록에 추가 */}
-                <div>
-                    <button>
-                        <VideoAddKeep />
-                        <span>나중에 볼 동영상</span>
-                    </button>
-                    <button>
-                        <VideoAddList />
-                        <span>목록에 추가</span>
-                    </button>
+                    {/* 영상 우측 클릭 영역 - 나중에 볼 동영상, 목록에 추가 */}
+                    <div className={css.video_item_button}>
+                        <button>
+                            <VideoAddKeep />
+                            <span className={css.video_item_button_text}>나중에 볼 동영상</span>
+                        </button>
+                        <button>
+                            <VideoAddList />
+                            <span className={css.video_item_button_text}>목록에 추가</span>
+                        </button>
+                    </div>
                 </div>
                 {/* 영상 설명 - profile */}
-                <div>
-                    <a href='#'>
+                <div className={css.video_item_desc}>
+                    <a href='#' className={css.video_item_desc_profile}>
                         <img src={profile} width="36" height="36" alt="" />
                         <span>{duration}</span>
                     </a>
-                    <div>
-                        <a href="#">{title}</a>
-                        <a href="#">
-                            <span>{channel.name}</span>
+                    <div className={css.video_item_desc_text}>
+                        <a href="#" className={css.video_item_desc_text_title}>{title}</a>
+                        <a href="#" className={css.video_item_desc_text_channel}>
+                            <span className={css.video_item_desc_text_channel_name}>{channel.name}</span>
                         </a>
-                        <a href="#">
+                        <a href="#" className={css.video_item_desc_text_hits}>
                             <span>{`조회수 ${viewCount}`}</span>
                             <span>{latestDate}</span>
                         </a>
