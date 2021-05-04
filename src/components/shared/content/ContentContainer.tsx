6468ad24550a317cf541e38e55b373c98e219c78
kkokkojeong <comgraphics11@gmail.com>
2021-05-04 21:18:09 +0900

    feat: #7 refactor advertisement

--- src/components/shared/content/ContentContainer.tsx
+++ src/components/shared/content/ContentContainer.tsx
@@ -1,14 +1,12 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import VideoItem from './video-item/VideoItem';
+import Advertisement from './advertisement/Advertisement';
 
 import mockContents from '../../../__mock__/contents.json';
 
 import css from './Content.module.scss'
 
-
 const ContentContainer = () => {
-    const [imgSrc] = useState<string>("https://www.gstatic.com/youtube/img/promos/growth/864dc47e45a7a0b06602f73155980de282b939a6d0adc7bdcda231f965bf796a_640x48.png");
-
     // mock-data memorizing
     const contents: any = useMemo(() => {
         const { contentList } = mockContents;
@@ -31,15 +29,8 @@ const ContentContainer = () => {
     return (
         <div className={css.container}>
             {/* 광고 관련 콘텐츠 */}
-            <div className={css.advertisement_container}>
-                <div className={css.advertisement_container_inner}>
-                    {/* Blind Text */}
-                    <h3 className={css.blind}>광고</h3>
-                    {/* 이미지 */}
-                    <img className={css.advertisement_container_image} src={imgSrc} alt="" />
-                    <strong className={css.advertisement_container_title}>좋⁠아⁠하⁠는 콘⁠텐⁠츠⁠를 광⁠고 없⁠이 즐⁠기⁠세⁠요</strong>
-                </div>
-            </div>
+            <Advertisement />
+
             {/* 동영상 콘텐츠 */}
             <div className={css.content_container}>
                 {/* Flow Section */}
--- src/components/shared/content/advertisement/Advertisement.tsx
+++ src/components/shared/content/advertisement/Advertisement.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReactComponent as CloseButton } from '../../../../assets/icons/CloseButton.svg';
 
-// TODO: ContentContainer 내 광고 콘텐츠 가져와서 리팩토링
+import css from '../Content.module.scss'
+
 const Advertisement = () => {
-    return <></>;
+    const [imgSrc] = useState<string>("https://www.gstatic.com/youtube/img/promos/growth/864dc47e45a7a0b06602f73155980de282b939a6d0adc7bdcda231f965bf796a_640x48.png");
+
+    return (
+        <>
+            {/* 광고 관련 콘텐츠 */}
+            <div className={css.advertisement_container}>
+                <div className={css.advertisement_container_inner}>
+                    {/* Blind Text */}
+                    <h3 className={css.blind}>광고</h3>
+                    {/* 이미지 */}
+                    <img className={css.advertisement_container_image} src={imgSrc} alt="" />
+                    <strong className={css.advertisement_container_title}>좋⁠아⁠하⁠는 콘⁠텐⁠츠⁠를 광⁠고 없⁠이 즐⁠기⁠세⁠요</strong>
+                    {/* 1개월 무료체험 */}
+                    <a href="#!" className={css.advertisement_container_free}>1개월 무료체험</a>
+                    {/* 닫기 버튼 */}
+                    <button className={css.advertisement_container_close_button}>
+                        <CloseButton />
+                        <span className={css.blind}>광고닫기</span>
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Advertisement;
